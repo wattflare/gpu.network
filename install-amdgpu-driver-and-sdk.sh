926681fd63bf3dfae6844a0ebc8b404c65234f0d
sanjay-shah <sanjay_y2k@yahoo.com>
2018-02-18 17:28:45 -0800

    platform upgrade

--- install-amdgpu-driver-and-sdk.sh
+++ install-amdgpu-driver-and-sdk.sh
@@ -1,6 +1,10 @@
-mkdir /tmp
-cd tmp
-wget --referer=http://support.amd.com  https://www2.ati.com/drivers/linux/ubuntu/amdgpu-pro-17.40-492261.tar.xz
-​tar -Jxvf amdgpu-pro-17.40-492261.tar.xz
-cd amdgpu-pro-17.40-492261
-amdgpu-pro-driver/amdgpu-pro-install
+sudo apt-get update \
+&& sudo apt-get upgrade -y \
+&& sudo apt-get dist-upgrade -y
+
+mkdir /amd_tmp
+cd amd_tmp
+wget --referer=http://support.amd.com  https://www2.ati.com/drivers/linux/ubuntu/amdgpu-pro-16.40-348864.tar.xz
+​tar -Jxvf amdgpu-pro-16.40-348864.tar.xz
+cd amdgpu-pro-16.40-348864
+#amdgpu-pro-driver/amdgpu-pro-install
