a829c7e27dbe8b0dbe404a10ee3f28ab604c014e
Sanjay Shah <sanjay@strongloop.com>
2018-02-18 16:25:47 -0800

    Create install-amdgpu-driver-and-sdk.sh

---
+++ install-amdgpu-driver-and-sdk.sh
@@ -0,0 +1,6 @@
+mkdir /tmp
+cd tmp
+wget --referer=http://support.amd.com  https://www2.ati.com/drivers/linux/ubuntu/amdgpu-pro-17.40-492261.tar.xz
+​tar -Jxvf amdgpu-pro-17.40-492261.tar.xz
+cd amdgpu-pro-17.40-492261
+amdgpu-pro-driver/amdgpu-pro-install
