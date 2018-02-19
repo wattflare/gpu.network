4297b001cfb4b08064848c5935bcd66d36b15149
sanjay-shah <sanjay_y2k@yahoo.com>
2018-02-18 17:53:09 -0800

    fixed tar syntax

--- install-amdgpu-driver-and-sdk.sh
+++ install-amdgpu-driver-and-sdk.sh
@@ -6,5 +6,4 @@ mkdir /amd_tmp
 cd amd_tmp
 wget --referer=http://support.amd.com  https://www2.ati.com/drivers/linux/ubuntu/amdgpu-pro-16.40-348864.tar.xz
 ​tar xvf amdgpu-pro-16.40-348864.tar.xz
-cd amdgpu-pro-16.40-348864
-#amdgpu-pro-driver/amdgpu-pro-install
+amdgpu-pro-16.40-348864/amdgpu-pro-install
