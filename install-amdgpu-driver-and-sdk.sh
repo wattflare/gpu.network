655acea8069f12ebabf8f1b65f755722a906be39
sanjay-shah <sanjay_y2k@yahoo.com>
2018-02-19 11:42:33 -0800

    added amd sdk

--- install-amdgpu-driver-and-sdk.sh
+++ install-amdgpu-driver-and-sdk.sh
@@ -2,10 +2,12 @@ sudo apt-get update \
 && sudo apt-get upgrade -y \
 && sudo apt-get dist-upgrade -y
 
-wget --referer=http://support.amd.com  https://www2.ati.com/drivers/linux/ubuntu/amdgpu-pro-16.40-348864.tar.xz
-tar xvf amdgpu-pro-16.40-348864.tar.xz
-amdgpu-pro-16.40-348864/amdgpu-pro-install -y
-wget https://github.com/wattflare/install-miners/releases/download/v3.0.130.136/AMD-APP-SDKInstaller-v3.0.130.136-GA-linux64.tar.bz2
-tar xf AMD-APP-SDKInstaller-v3.0.130.136-GA-linux64.tar.bz2
-sudo ./AMD-APP-SDK-v3.0.130.136-GA-linux64.sh
+wget --referer=http://support.amd.com  https://www2.ati.com/drivers/linux/ubuntu/amdgpu-pro-16.40-348864.tar.xz \
+&& tar xvf amdgpu-pro-16.40-348864.tar.xz \
+&& amdgpu-pro-16.40-348864/amdgpu-pro-install -y
+
+wget https://github.com/wattflare/install-miners/releases/download/AMD-SDK/AMD-APP-SDKInstaller-v3.0.130.136-GA-linux64.tar.bz2 \
+&& tar xf AMD-APP-SDKInstaller-v3.0.130.136-GA-linux64.tar.bz2 \
+&& sudo ./AMD-APP-SDK-v3.0.130.136-GA-linux64.sh
+
 sudo usermod -a -G video $LOGNAME
