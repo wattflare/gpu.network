235eb9f82a93fdd5e8a3a01a5f3833fc89475d6f
Sanjay Shah <sanjay_y2k@yahoo.com>
2018-05-07 20:35:03 -0700

    added support for ETH chains

--- install-miner-amd.sh
+++ install-miner-amd.sh
@@ -1,16 +1,23 @@
-COIN_ENABLED=${COIN_ENABLED:-"zcl"}
-
+COIN_ENABLED=${COIN_ENABLED:-"music"}
+#equihash
 VOT_ADDR=${VOT_ADDR:-"t1ZKv84aTkcT1kvnMiY49oUgFS72Xm8jHae"}
 ZCL_ADDR=${ZCL_ADDR:-"t1MDmBLNnHao3CSL57NU41fwRKEsN4V9PyX"}
 ZEN_ADDR=${ZEN_ADDR:-"GET_NEW_ADDRESS"}
 HUSH_ADDR=${HUSH_ADDR:-"t1WvpdsV5HZjdNuzLhngeS6ecfGjut94c3u"}
 BTCZ_ADDR=${BTCZ_ADDR:-"t1Yc53Y7Sm96bxdmV9yC2tgEQNaHDC6T4SD"}
+#ethash
+MUSIC_ADDR=${MUSIC_ADDR:-"0x0aedeb18ed2170318b07250bf5e525dfa19104d1"}
+ETH_ADDR=${MUSIC_ADDR:-"0x0aedeb18ed2170318b07250bf5e525dfa19104d1"}
 
+#equihash
 VOT_POOL="165.227.12.162:8034"
 ZCL_POOL="165.227.12.162:3034"
 ZEN_POOL="165.227.12.162:4034"
 HUSH_POOL="165.227.12.162:7034"
 BTCZ_POOL="165.227.12.162:5034"
+#ethash
+MUSIC_POOL="stratum+tcp://music.gpu.network:8008"
+ETH_POOL="stratum+tcp://music.gpu.network:9009"
 
 current_user=$USER
 current_dir=$PWD
@@ -18,10 +25,12 @@ miners_dir="$HOME/wtf_miners"
 
 mkdir $miners_dir && cd $miners_dir 
 git clone https://github.com/wattflare/claymore-zcash.git
+git clone https://github.com/wattflare/clamore-eth.git
 cd $current_dir
 
 #create coin mining scripts
 
+#equihash
 echo "$miners_dir/claymore-zcash/zecminer64 -zpool $VOT_POOL  -zwal $VOT_ADDR.$HOSTNAME -zpsw x" > $miners_dir/mine.vot.pool.wattflare.com.sh
 
 echo "$miners_dir/claymore-zcash/zecminer64 -zpool $ZCL_POOL  -zwal $ZCL_ADDR.$HOSTNAME -zpsw x" > $miners_dir/mine.zcl.pool.wattflare.com.sh
@@ -31,6 +40,10 @@ echo "$miners_dir/claymore-zcash/zecminer64 -zpool $ZEN_POOL  -zwal $ZEN_ADDR.$H
 echo "$miners_dir/claymore-zcash/zecminer64 -zpool $HUSH_POOL  -zwal $HUSH_ADDR.$HOSTNAME -zpsw x" > $miners_dir/mine.hush.pool.wattflare.com.sh
 
 echo "$miners_dir/claymore-zcash/zecminer64 -zpool $BTCZ_POOL  -zwal $BTCZ_ADDR.$HOSTNAME -zpsw x" > $miners_dir/mine.btcz.pool.wattflare.com.sh
+
+#ethash
+echo "$miners_dir/claymore-eth/ethdcrminer64 -epool $MUSIC_POOL  -ewal $MUSIC_ADDR.$HOSTNAME -epsw x" > $miners_dir/mine.music.pool.wattflare.com.sh -allpools 1
+echo "$miners_dir/claymore-eth/ethdcrminer64 -epool $ETH_POOL  -ewal $ETH_ADDR.$HOSTNAME -epsw x" > $miners_dir/mine.eth.pool.wattflare.com.sh
 
 #make mining scripts executable
 chmod +x $miners_dir/mine.*.pool.wattflare.com.sh
