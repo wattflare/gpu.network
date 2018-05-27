39ee0450bdb24e72a97bb6a43ef6827ef6889cf2
Sanjay Shah <sanjay_y2k@yahoo.com>
2018-05-27 03:34:18 -0700

    Create overclock_power_limit_amd.sh

---
+++ overclock_power_limit_amd.sh
@@ -0,0 +1,10 @@
+#echo "manual" > /sys/class/drm/card8/device/power_dpm_force_performance_level
+#/opt/ohgodatool/ohgodatool -i 8 --set-fanspeed 60 --core-state 3 --core-clock 1050
+#echo 3 > /sys/class/drm/card8/device/pp_dpm_sclk
+#/opt/ohgodatool/ohgodatool -i 8 --set-max-power 110
+#/opt/ohgodatool/ohgodatool -i 8 --mem-state "2" --mem-clock "1950"
+
+#The following commands must be run while logged in as root. sudo will not work!
+
+#echo "3" > /sys/class/drm/card8/device/pp_dpm_sclk
+#echo "manual" > /sys/class/drm/card8/device/power_dpm_force_performance_level
