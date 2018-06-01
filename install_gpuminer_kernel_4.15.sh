797281e613ebde9c712fb9f48e6ab62d6e71ffe3
Sanjay Shah <sanjay_y2k@yahoo.com>
2018-06-01 01:07:56 -0700

    Update install_gpuminer_kernel_4.15.sh

--- install_gpuminer_kernel_4.15.sh
+++ install_gpuminer_kernel_4.15.sh
@@ -13,6 +13,10 @@ wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15/linux-image-4.15.0-0415
 sudo dpkg -i *.deb
 
 git clone https://github.com/OhGodACompany/OhGodATool.git
+cd OhGodATool
+make
+cd ..
+
 
 wget https://github.com/d13g0s0uz4/atiflash/releases/download/0.1/atiflash_linux.tar.xz -O /tmp/atiflash_linux.tar.xz && sudo tar xvf /tmp/atiflash_linux.tar.xz -C /usr/local/bin/
 
