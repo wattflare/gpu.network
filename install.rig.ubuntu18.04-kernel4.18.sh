a6c43959c998b5ede93bedfdc3d38031ec5a6e22
Sanjay Shah <sanjay@strongloop.com>
2019-06-28 00:19:08 -0700

    Create install.rig.ubuntu18.04-kernel4.18.sh

---
+++ install.rig.ubuntu18.04-kernel4.18.sh
@@ -0,0 +1,9 @@
+sudo apt-get update && sudo apt-get upgrade -y
+cd /tmp/
+wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18/linux-headers-4.18.0-041800_4.18.0-041800.201808122131_all.deb
+wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18/linux-headers-4.18.0-041800-generic_4.18.0-041800.201808122131_amd64.deb
+wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18/linux-image-unsigned-4.18.0-041800-generic_4.18.0-041800.201808122131_amd64.deb
+wget -c http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18/linux-modules-4.18.0-041800-generic_4.18.0-041800.201808122131_amd64.deb
+sudo dpkg -i *.deb
+
+sudo reboot
