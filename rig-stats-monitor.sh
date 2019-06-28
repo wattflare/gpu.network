16b61cc11ed7cedfbde056659088eb93ed260c19
Sanjay Shah <sanjay@strongloop.com>
2019-06-28 05:53:31 -0700

    nc wait timeout

--- rig-stats-monitor.sh
+++ rig-stats-monitor.sh
@@ -1,1 +1,1 @@
-echo '{"id":0,"jsonrpc":"2.0","method":"miner_getstatdetail"}' | nc 127.0.0.1 3333 | jq  '.result' | curl -H "Content-Type: application/json" -X POST -d @- http://api.gpu.network/api/rigs
+echo '{"id":0,"jsonrpc":"2.0","method":"miner_getstatdetail"}' | nc 127.0.0.1 3333 -w 1 | jq  '.result' | curl -H "Content-Type: application/json" -X POST -d @- http://api.gpu.network/api/rigs
