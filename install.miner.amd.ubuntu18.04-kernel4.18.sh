2e77abdd5b59774916f856a135ea35107c7e2112
Sanjay Shah <sanjay@strongloop.com>
2019-06-28 23:54:04 -0700

    Update install.miner.amd.ubuntu18.04-kernel4.18.sh

--- install.miner.amd.ubuntu18.04-kernel4.18.sh
+++ install.miner.amd.ubuntu18.04-kernel4.18.sh
@@ -2,4 +2,5 @@ sudo apt update
 sudo apt install libnuma-dev -y
 wget -qO - http://repo.radeon.com/rocm/apt/debian/rocm.gpg.key | sudo apt-key add -
 echo 'deb [arch=amd64] http://repo.radeon.com/rocm/apt/debian/ xenial main' | sudo tee /etc/apt/sources.list.d/rocm.list
+sudo apt update
 sudo apt-get install rocm-opencl-dev
